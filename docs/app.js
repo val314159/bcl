96665e31bfc09613fe02c6d16b9d70750c0eabb1
Joel Ward <jmward@gmail.com>
2021-05-27 19:29:01 -0500

    update

--- docs/app.js
+++ docs/app.js
@@ -1,2 +1,38 @@
 // app.js
-pr(";; app.js")
+const outElt = GEBI("out")
+function out(x){outElt.appendChild(D.createTextNode(x))}
+function  pr(x){out(x+"\n");return x}
+function   p(x){out(x     );return x}
+function keydown(t,e){
+    if(e.code!='Enter')return
+    if(!e.altGraphKey&&
+       !e.altKey&&
+       e.ctrlKey&&
+       !e.metaKey&&
+       !e.shiftKey){
+	cl("SUBMIT THIS")
+	var value = replace(t,'value','')
+	cl("SUBMIT THIS " + value)
+	const ws = new PubSubSocket("/bongo")
+	ws.onopen = function(){
+	    cl("OPN")
+	    ws.sub("x b u")}
+	ws.onclose = function(){
+	    cl("CLS")
+	}
+	ws.onmessage = function(e){
+	    const data = e.data
+	    cl(["DATA:",data])
+	    if(!value) ws.close()
+	    else{ws.pub(value)
+		 value = ""}}
+	return false}
+    if(!e.altGraphKey&&
+       e.altKey&&
+       !e.ctrlKey&&
+       !e.metaKey&&
+       !e.shiftKey){
+	cl("EVALUATE THIS")
+	const value = replace(t,'value','')
+	cl("EVALUE THIS " + value)
+	return false}}
--- docs/util.js
+++ docs/util.js
@@ -1,4 +1,10 @@
 // util.js
 const D=document
-function pr(x){console.log(x)}
-pr(";; util.js")
+function cl(x){console.log(x)}
+function GEBI(x){return D.getElementById(x)}
+function replace(x,k,v){const r = x[k];x[k] = v;return r}
+function PubSubSocket(path){
+    const ws = new WebSocket(`ws://${location.host}${path}`)
+    ws.pub = function(ch, msg){ ws.send(`(pub 1 "${ch}")`), ws.send(msg)}
+    ws.sub = function(channels){ws.send(`(sub "${channels}")`)}
+    return ws}
